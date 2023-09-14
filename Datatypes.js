2fea62d3303bfac1122082fa1b624bdec1a3c693
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2023-09-14 15:36:13 +0530

    DataTypes in JS

---
+++ Datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //It is write because the JS engine treat all JS code as newer version.
+
+// alert(3+3); //It gives an error alert is not defined. Because we are using node, not browser.Js uses browser.
+
+//Primitive DataType.
+
+let name = "Mayur"; //String DataType.
+let age = 22; //It is number DataType.
+let isLoggedIn = true; //Boolean DataType.
+ null; // It is a stand alone value. It is blank.
+undefined; // We declare a variable but not define variable.
+Symbol; //For unique we use symbol Datatype.
+
+//Non-Primitive DataType.
+
+console.log(typeof "Mayur");
+console.log(typeof(age));  //console.log(typeof age);
+console.log(typeof undefined);  //Output is undefined.
+console.log(typeof null);  //Output is object.
+
