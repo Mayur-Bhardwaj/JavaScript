7412100db775d523b7215f1b8d79833485244e0e
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2023-09-25 18:06:50 +0530

    Add files via upload

---
+++ Operatons.js
@@ -0,0 +1,31 @@
+// let value = 4;
+// let negative_value = -value;
+// // console.log(negative_value);
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2/2);
+// // console.log(2+-2); //0
+// // console.log(2**2);
+// // console.log(2%2);
+
+// let str1 = "Hello";
+// let str2 = " Mayur";
+// let str3 = str1 + str2;
+// console.log(str3);
+// // console.log(str1 + str2);
+
+// console.log(1 + "2"); //12
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+console.log(true); //true
+console.log(+true); //1
+// console.log(true+); //Error
+
+let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter); //101
+++gameCounter;
+console.log(gameCounter); //101
