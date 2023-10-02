290c6a01519fdb0a83459745332b10b1b3d6b8ed
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2023-10-02 15:21:27 +0530

    Add files via upload

---
+++ Callback.js
@@ -0,0 +1,12 @@
+// Arrow functions.
+//Callback functions --> how we send function inside function.
+
+function greet (){
+    console.log("Function");
+}
+greet();
+  
+const greet1 = () => {
+    console.log("Arrow Function");
+}
+greet1();
